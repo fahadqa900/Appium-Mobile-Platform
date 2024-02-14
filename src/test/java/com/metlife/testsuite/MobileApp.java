cb4f9c3bbdfcaa31a81b5a5252e923edf607da1e
unknown <fahadqa900@gmail.com>
2024-02-14 15:20:34 +0530

    Committed by fahad ali khan

---
+++ src/test/java/com/metlife/testsuite/MobileApp.java
@@ -0,0 +1,22 @@
+package com.metlife.testsuite;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.NetworkSpeed;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MobileApp
+{
+
+    public static void main(String[] args) throws MalformedURLException {
+        UiAutomator2Options options=new UiAutomator2Options()
+                .setNetworkSpeed(NetworkSpeed.EDGE.name())
+                .setApp("C:\\Users\\Administrator\\Downloads\\KA.apk");
+        AndroidDriver driver=new AndroidDriver(new URL("127.0.0.1:4723)"),options);
+
+
+
+    }
+}
